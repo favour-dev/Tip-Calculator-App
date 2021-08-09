659b800e957c0b734a8471eaff1541111c8fd257
favour <f.ebebeinwe@gmail.com>
2021-08-09 15:34:40 +0100

    Fix: Input Placeholder

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 document.getElementById("calculate").addEventListener('click', calculateTip)
-
+// Getting the service tip value and storing it in a variable which will change dynamically based on user interaction.
 let serviceTip = 0;
 document.getElementById("five").addEventListener('click', () => serviceTip = 5)
 document.getElementById("ten").addEventListener('click', () => serviceTip = 10)
@@ -8,22 +8,12 @@ document.getElementById("twenty").addEventListener('click', () => serviceTip = 2
 document.getElementById("fifty").addEventListener('click', () => serviceTip = 50)
 document.getElementById("serviceCharge").addEventListener('keyup', () => serviceTip = document.getElementById('serviceCharge').value)
 
-// document.getElementsByClassName('profit-increase').onclick = changeColor;
-// function changeColor(){
-//     document.style.backgroundColor = "red"; 
-// }
-
-
-
 function calculateTip(){
 // gets user input from the dom
-    // console.log(`The value of service tip is ${serviceTip}`)
-
     let userBill = document.getElementById("bill").value
     let numberOfPeople = document.getElementById("people").value
-    // let serviceTip = document.getElementById('serviceCharge').value
-
-    if(userBill === "" || numberOfPeople === "" ){
+// validate user input
+    if(userBill === "" || numberOfPeople === "" || serviceTip === "" ){
         alert('All Input fields must be filled')
     }else{
 // calculates tip per person
@@ -33,7 +23,7 @@ function calculateTip(){
 // calulates total per person
     let total = (parseInt(userBill) / numberOfPeople) + parseInt(tipAmount);
     total = total.toFixed(2);
-    document.getElementById('total-fee').innerHTML = "$"+total
+    document.getElementById('total-fee').innerHTML = "$"+total;
        
     }
 
